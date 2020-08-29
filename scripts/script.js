07b140a6594c4dae622e529cf5ac3e5b43543d1c
Limon1980 <tarkalmob@yandex.ru>
2020-08-29 19:08:27 +1000

    Урок 16-1 codeopen 1 выполнил

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,39 @@
 'use strict';
 
-class First {
-  hello() {
-    console.log('Привет я метод родителя!');
+const myLesson = [
+  { lesson: 1, type: 'basic', points: 2 },
+  { lesson: 2, type: 'additional', points: 4 },
+  { lesson: 3, type: 'basic', points: 6 },
+  { lesson: 4, type: 'additional', points: 3 },
+  { lesson: 5, type: 'basic', points: 4 },
+  { lesson: 6, type: 'basic', points: 2 },
+  { lesson: 7, type: 'additional', points: 2 },
+  { lesson: 8, type: 'basic', points: 6 },
+  { lesson: 9, type: 'basic', points: 4 },
+  { lesson: 10, type: 'basic', points: 6 },
+  { lesson: 11, type: 'additional', points: 5 },
+  { lesson: 12, type: 'basic', points: 2 },
+  { lesson: 13, type: 'additional', points: 2 },
+  { lesson: 14, type: 'basic', points: 4 },
+  { lesson: 15, type: 'additional', points: 1 },
+  { lesson: 16, type: 'additional', points: 7 },
+];
+
+// console.log(myLesson);
+
+const foo = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let { type, points } = arr[i];
+
+    if (type === 'additional') {
+      myLesson.splice(i, 1); // почему не удаляет последний элемент
+      i--;
+    } else {
+      const obj = { points: points / 2 };
+      Object.assign(arr[i], obj);
+    }
   }
-}
+  return arr;
+};
 
-class Second extends First {
-  hello() {
-    super.hello();
-    console.log('А я наследуемый метод!');
-  }
-}
-
-let second = new Second();
-
-second.hello();
+console.log(foo(myLesson));
