c82da3dfd8297e93bd0e039a6839b3424dddf01e
Limon1980 <tarkalmob@yandex.ru>
2020-09-07 21:22:26 +1000

    2 подпункт время в тег <b></b>

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,22 @@
 
 const task1 = document.getElementById('task1');
 
-const res = task1.innerHTML.replace(/(функци)([ия])?/g, (match, val1, val2) => {
-  // console.log(match, val1, val2);
-  // console.log(`<strong>${val1 + val2}</strong>`);
-  return `<strong>${val1 + val2}</strong>`;
-});
+const res1 = task1.innerHTML.replace(
+  /(функци)([ия])?/g,
+  (match, val1, val2) => {
+    // console.log(match, val1, val2);
+    // console.log(`<strong>${val1 + val2}</strong>`);
+    return `<strong>${val1 + val2}</strong>`;
+  }
+);
 
-task1.innerHTML = res;
+task1.innerHTML = res1;
+
+const task2 = document.getElementById('task2');
+
+let res2 = (task2.innerHTML = task2.innerHTML.replace(/<b>|<\/b>/g, '')); // удалим все теги <b></b>;
+
+res2 = res2.replace(/(\d{2}):(\d{2})/g, (match) => {
+  return `<b>${match}</b>`;
+});
+task2.innerHTML = res2;
